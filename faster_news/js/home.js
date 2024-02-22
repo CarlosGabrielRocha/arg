d0f1f78fcc3fed20812be6d82916c4761ea55116
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-02-22 13:15:05 -0300

    faster news/ piadas v0.1

--- faster_news/js/home.js
+++ faster_news/js/home.js
@@ -85,10 +85,31 @@ function Tamanho() {
     }
 }
 
-//audio
+//piadas.html
 
 
-function ExecAudio() {
-    var play = window.document.getElementById('playaudio')
-    playaudio.play()
+function resposta1() {
+    let res = document.querySelector('div.resposta1')
+    let video = document.querySelector('iframe.rir1')
+    if (video.style.display == 'none') {
+        res.innerHTML = `Quando ela está aprendendo uma nova língua.`
+        video.style.display = 'block'
+    } else {
+        res.innerHTML = 'Resposta'
+        video.style.display = 'none'
+    }
+    
+}
+
+function resposta2() {
+    let res = document.querySelector('div.resposta2')
+    let video = document.querySelector('iframe.rir2')
+    if (video.style.display == 'none') {
+        res.innerHTML = `Elas contam PIAdas`
+        video.style.display = 'block'
+    } else {
+        res.innerHTML = 'Resposta'
+        video.style.display = 'none'
+    }
+    
 }
