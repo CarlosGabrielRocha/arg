57d760add10472e2170053d4d27c6970a7fb469c
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-02-27 13:16:42 -0300

    ....

--- faster_news/js/home.js
+++ faster_news/js/home.js
@@ -84,32 +84,3 @@ function Tamanho() {
         opcoes.style.display = 'none'
     }
 }
-
-//piadas.html
-
-
-function resposta1() {
-    let res = document.querySelector('div.resposta1')
-    let video = document.querySelector('iframe.rir1')
-    if (video.style.display == 'none') {
-        res.innerHTML = `Quando ela está aprendendo uma nova língua.`
-        video.style.display = 'block'
-    } else {
-        res.innerHTML = 'Resposta'
-        video.style.display = 'none'
-    }
-    
-}
-
-function resposta2() {
-    let res = document.querySelector('div.resposta2')
-    let video = document.querySelector('iframe.rir2')
-    if (video.style.display == 'none') {
-        res.innerHTML = `Elas contam PIAdas`
-        video.style.display = 'block'
-    } else {
-        res.innerHTML = 'Resposta'
-        video.style.display = 'none'
-    }
-    
-}
---
+++ faster_news/js/receitas.js
@@ -0,0 +1,4 @@
+
+function escolheu() {
+    alert('[CONTÉUDO INDISPONÍVEL] NO MOMENTO! ')
+}
