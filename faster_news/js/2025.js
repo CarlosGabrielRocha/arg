ffc1566b3b1489eaf5eb88ee9dc7be68ee5bb201
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-03-16 20:01:44 -0300

    faster news v1.0

--- faster_news/js/2025.js
+++ faster_news/js/2025.js
@@ -27,3 +27,20 @@ function formatTime(time) {
 }
 
 setInterval(countdown, 1000)
+
+//menu
+
+function ClickMenu(){
+
+    if (opcoes.style.display == 'block'){
+        opcoes.style.display = 'none';
+    } else {
+        opcoes.style.display = 'block';
+    }
+}
+
+function Tamanho() {
+    if (window.innerWidth > 768) {
+        opcoes.style.display = 'none';
+    }
+}
