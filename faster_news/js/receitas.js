e45ea4408dce4f5a09012430cf2674de9a1d1ab8
Carlos Gabriel <Gabrielrocha092003@gmail.com>
2024-03-16 20:06:59 -0300

    faster news/ mudanças simples no javaScript

--- faster_news/js/receitas.js
+++ faster_news/js/receitas.js
@@ -1,4 +1,21 @@
 
 function escolheu() {
-    alert('[CONTÉUDO INDISPONÍVEL] NO MOMENTO! ')
+    alert('[CONTÉUDO INDISPONÍVEL] NO MOMENTO! ');
+}
+
+//menu
+
+function ClickMenu(){
+
+    if (opcoes.style.display == 'block'){
+        opcoes.style.display = 'none';
+    } else {
+        opcoes.style.display = 'block';
+    }
+}
+
+function Tamanho() {
+    if (window.innerWidth > 768) {
+        opcoes.style.display = 'none';
+    }
 }
