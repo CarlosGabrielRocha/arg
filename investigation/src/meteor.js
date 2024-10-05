be29066d2be600038cff6a20677950f154790539
Carlos Gabriel <148346104+CarlosGabrielRocha@users.noreply.github.com>
2024-10-05 16:02:53 -0300

    tentativa 2 de animação do meteoro nos celulares

--- investigation/src/meteor.js
+++ investigation/src/meteor.js
@@ -1,7 +1,15 @@
+const containerForMeteor = document.createElement('section')
+containerForMeteor.style.position = 'absolute'
+containerForMeteor.style.width = '100vw'
+containerForMeteor.style.height = '100vh'
+containerForMeteor.style.overflow = 'hidden'
+containerForMeteor.style.zIndex = '-1'
+
 const meteor = document.createElement('div')
 meteor.id = 'meteor'
-meteor.style.zIndex = '-1'
-document.body.appendChild(meteor)
+
+document.body.appendChild(containerForMeteor)
+containerForMeteor.appendChild(meteor)
 
 
 function restartMeteorAnimation() {
